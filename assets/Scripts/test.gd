ec08d4cbef67b4973db3cb8f5eb101dff2e8295e
KShaker <77405346+NN77296@users.noreply.github.com>
2024-04-04 11:30:43 -0500

    so like basically i found an interesting artcile
    like ghuys its so cool you should read it its in test.gd in scritps folder

--- assets/Scripts/test.gd
+++ assets/Scripts/test.gd
@@ -5,6 +5,7 @@ extends Node3D
 func _ready():
 	pass
 
+# https://www.theguardian.com/society/2024/apr/02/new-science-of-death-brain-activity-consciousness-near-death-experience?utm_source=pocket-newtab-en-us
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
