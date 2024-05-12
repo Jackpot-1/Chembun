7d6f1321ebea8595a92bf032fbec3943f72ae2ab
Jackpot-1 <142419800+Jackpot-1@users.noreply.github.com>
2024-05-11 21:40:35 -0500

    put chembun in the cave, moved hotbar into canvas layer, and removed the materials put on slot 1 and 5

--- assets/GUI.gd
+++ assets/GUI.gd
@@ -17,13 +17,13 @@ func _process(delta):
 func stop_player_input():
 	playerStopped = true
 	DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_VISIBLE) # unhides the mouse and lets it move
-	$"../PlayerTemplate/Camroot".process_mode = Node.PROCESS_MODE_DISABLED
-	$"../PlayerTemplate/CamRightTank".process_mode = Node.PROCESS_MODE_DISABLED
+	$"../ChemBun/Camroot".process_mode = Node.PROCESS_MODE_DISABLED
+	$"../ChemBun/CamRightTank".process_mode = Node.PROCESS_MODE_DISABLED
 func unstop_player_input():
 	playerStopped = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) # hides the mouse and makes it so it can't move
-	$"../PlayerTemplate/Camroot".process_mode = Node.PROCESS_MODE_INHERIT
-	$"../PlayerTemplate/CamRightTank".process_mode = Node.PROCESS_MODE_INHERIT
+	$"../ChemBun/Camroot".process_mode = Node.PROCESS_MODE_INHERIT
+	$"../ChemBun/CamRightTank".process_mode = Node.PROCESS_MODE_INHERIT
 
 func _input(event):
 	if event.is_action_pressed("menu"):
