b8224973aa19b42ddba15f5e09faad7535b766a4
Jackpot-1 <142419800+Jackpot-1@users.noreply.github.com>
2024-05-13 20:43:46 -0500

    added fullscreen

--- assets/Scripts/autoloads/Globals.gd
+++ assets/Scripts/autoloads/Globals.gd
@@ -9,11 +9,13 @@ var hasKey = false
 
 func _ready():
 	load_data()
-	
+
 func _unhandled_input(event):
 	if event.is_action_pressed("fullscreen"):
-		#OS.window_fullscreen = !OS.window_fullscreen
-		pass
+		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 
 var player: CharacterBody3D;
 
