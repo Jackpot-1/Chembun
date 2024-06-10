902685ea80df4f354acd1508b74c1a8c93d67f1e
Jackpot-1 <142419800+Jackpot-1@users.noreply.github.com>
2024-06-10 14:41:30 -0500

    Collision with the water works, turns out it had to be a convex collision shape instead of concave, the reason why it used concave is because the model goes down and back up or something

---
+++ assets/fuk/test.gd
@@ -0,0 +1,27 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_static_body_3d_body_entered(body):
+	print("entered")
+
+
+func _on_static_body_3d_body_exited(body):
+	print("exited")
+
+
+func _on_area_3d_body_entered(body):
+	print("entered")
+
+
+func _on_area_3d_body_exited(body):
+	print("exited")
